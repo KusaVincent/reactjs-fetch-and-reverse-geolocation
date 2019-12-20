fe9cf43e2a92cfa8b00ead8e1d6fcac25530155f
KusaVincent <vincentkusa170203@daystar.ac.ke>
2019-12-20 04:08:37 +0300

    implemented the fetch api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Fetch from "./component/fetch";
+// import { google_map_api_key } from "../../model/config";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      latitude: null,
+      longitude: null,
+      userAddress: null
+    };
+  }
+
+  getLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(this.getCoordinates);
+    } else {
+      alert("not supported");
+    }
+  };
+
+  getCoordinates = position => {
+    this.setState({
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    });
+    // this.reverseGeocoding();
+  };
+
+  // reverseGeocoding = () => {
+  //   const lat = this.state.latitude;
+  //   const lng = this.state.longitude;
+  //   fetch(
+  //     `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${config.google_map_api_key}`
+  //   )
+  //     .then(response => response.json())
+  //     .then(
+  //       data => console.log(data)
+  //       // this.setState({
+  //       //   userAddress: data.results[0].formatted_address
+  //       // })
+  //     )
+  //     .then.catch(error => alert(error));
+  // };
+
+  handleLocationError = error => {
+    switch (error.code) {
+      case error.PERMISSION_DENIED:
+        alert("denied request");
+        break;
+      case error.POSSITION_UNAVAILABLE:
+        alert("unavailable request");
+        break;
+      case error.TIMEOUT:
+        alert("request timeout");
+        break;
+      case error.UNKNOWN_ERROR:
+        alert("unknown request");
+        break;
+      default:
+        alert("unknown error");
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.getLocation}>Location</button>
+        <p>latitude: {this.state.latitude}</p>
+        <p>longitude: {this.state.longitude}</p>
+        {/* <p>userAddress:{this.state.userAddress}</p> */}
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          userAddress:
+          <Fetch />
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        {this.state.latitude && this.state.longitude ? (
+          <img src={``} alt="" />
+        ) : null}
+        {/* https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${config.google_map_api_key} */}
+      </div>
+    );
+  }
 }
-
 export default App;
---
+++ src/component/fetch.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+
+class Fetch extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: [],
+      isLoaded: false
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/Users")
+      .then(response => response.json())
+      .then(json => {
+        this.setState({
+          isLoaded: true,
+          value: json
+        });
+      });
+  }
+
+  render() {
+    let { isLoaded, value } = this.state;
+
+    if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <div>
+          <ul>
+            {" "}
+            {value.map(item => (
+              <li key={item.id}>username: {item.username}</li>
+            ))}
+            
+          </ul>
+        </div>
+      );
+    }
+  }
+}
+export default Fetch;
